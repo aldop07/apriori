091c123e3a32741dbb95842180115775276f7428
Aldo <30534322+aldop07@users.noreply.github.com>
2023-01-15 00:12:20 +0700

    Update apriori.py

--- apriori.py
+++ apriori.py
@@ -9,33 +9,41 @@ st.header('Apriori')
 uploaded_file = st.file_uploader("Pilih file Excel yang akan diinputkan:", type=["xlsx"])
 if uploaded_file is None:
     st.write("Tidak ada file yang diupload.")
+    return
+else:
+    if uploaded_file.name.split(".")[-1] != "xlsx":
+        st.error("File yang diupload harus berformat excel")
+        return
+try:
+    df = pd.read_excel(uploaded_file)
+except Exception as e:
+    st.error("Terjadi kesalahan saat membaca file, silahkan cek kembali file yang diupload")
+    return
 
-df = pd.read_excel(uploaded_file)
- 
 # Buat menjadi tabulasi data
 A = st.text_input ('Masukan Index A')
 B = st.text_input ('Masukan Index B')
 
-tabular = pd.crosstab (df[A],df[B])
-
-# Data dibaca dengan cara encoding
-def hot_encode(x) :
-        if (x<=0):
-            return 0
-        if (x>=1):
-            return 1
-
-# Buat data menjadi binominal
-tabular_encode = tabular.applymap(hot_encode)
+try:
+    if (A in df.columns) and (B in df.columns):
+        tabular = pd.crosstab(df[A], df[B])
+    else:
+        st.error("Index tidak ditemukan")
+        return
+except NameError:
+    st.error("Dataframe tidak ditemukan")
+    return
 
 # Menentukan nilai minimum support
 minimum_support = st.number_input("Nilai minimum support:",0.01)
 if minimum_support <= 0:
     st.warning("Nilai minimum support tidak boleh kosong atau nol.")
+    return
     
 minimum_confidence = st.number_input("Nilai minimum confidence:",0.01)
 if minimum_confidence <= 0:
     st.warning("Nilai minimum confidence tidak boleh kosong atau nol.")
+    return
 
 # Bangun model apriori
 frq_items = apriori(tabular_encode, min_support=minimum_support, use_colnames= True)
@@ -44,7 +52,6 @@ frq_items = apriori(tabular_encode, min_support=minimum_support, use_colnames= T
 rules = association_rules(frq_items, metric="lift",min_threshold=minimum_confidence)
 rules = rules.sort_values(['confidence','lift'], ascending=[False, False])
 
-# Menampilkan hasil algoritma apriori
 if st.button("PROSES"):
     st.success('HASIL PERHITUNGAN APRIORI')
         
@@ -53,3 +60,4 @@ if st.button("PROSES"):
     
     # Menampilkan hasil algoritma apriori dalam bentuk dataframe
     st.dataframe(rules.applymap(lambda x: ','.join(x) if type(x) == frozenset else x))
+
