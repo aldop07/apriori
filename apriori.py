d62c7d166f2fd7451f64841c5172fbbfdd15d849
Aldo <30534322+aldop07@users.noreply.github.com>
2023-01-21 08:14:27 +0700

    Update apriori.py

--- apriori.py
+++ apriori.py
@@ -19,7 +19,6 @@ if uploaded_file:
     if A not in df.columns or B not in df.columns:
         st.warning("Index yang Anda masukkan tidak ditemukan dalam file yang diupload")
     else: 
-
         tabular = pd.crosstab (df[A],df[B])
 
         # Data dibaca dengan cara encoding
@@ -32,24 +31,22 @@ if uploaded_file:
         # Buat data menjadi binominal
         tabular_encode = tabular.applymap(hot_encode)
 
+   # Menampilkan hasil algoritma apriori
+    if st.button("PROSES"):
+        st.success('HASIL PERHITUNGAN APRIORI')
         # Bangun model apriori
         frq_items = apriori(tabular_encode, min_support=minimum_support, use_colnames= True)
 
         # Mengumpulkan aturan dalam dataframe
         rules = association_rules(frq_items, metric="lift",min_threshold=minimum_confidence)
         rules = rules.sort_values(['confidence','lift'], ascending=[False, False])
-
-   # Menampilkan hasil algoritma apriori
-    if st.button("PROSES"):
-       
-        st.warning("aturan asosiasi tidak dapat diproses")
-
-    else:
-        st.success('HASIL PERHITUNGAN APRIORI')
+        
         # Mengubah nilai support, confidence, dan lift menjadi persentase
         rules[["antecedent support","consequent support","support","confidence"]] = rules[["antecedent support","consequent support","support","confidence"]].applymap(lambda x: "{:.2f}%".format(x*100))
 
         # Menampilkan hasil algoritma apriori dalam bentuk dataframe
         st.dataframe(rules.applymap(lambda x: ','.join(x) if type(x) == frozenset else x))
+    else:
+        st.warning("aturan asosiasi tidak dapat diproses")
 else:
     st.write("Tidak ada file yang diupload.")
