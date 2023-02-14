71de53db7cf4b3a9d3d815cffb26c0b100226048
Aldo <30534322+aldop07@users.noreply.github.com>
2023-02-14 12:17:13 +0700

    Update apriori.py

--- apriori.py
+++ apriori.py
@@ -10,17 +10,26 @@ st.set_page_config(page_title="MBA", page_icon=icon)
 st.header('Market Basket Analysis')
 
 # Baca data transaksi dari database
-uploaded_file = st.file_uploader("Pilih file Excel yang akan diupload:")
+uploaded_file = st.file_uploader("Pilih file Excel yang diupload:")
 if uploaded_file:
     df = pd.read_excel(uploaded_file)
     index_list = df.columns.tolist()
     A = st.selectbox ('X / Invoice',index_list)
     B = st.selectbox ('Y / Product',index_list)
+
+    all = st.checkbox('Pilih Berdasarkan Tanggal')
+    if all:
+        C = st.selectbox('Pilih Kolom Tanggal', index_list)
+        df[C] = pd.to_datetime(df[C])  # Mengubah kolom tanggal menjadi tipe datetime
+        tanggal_mulai = st.date_input("Tanggal Mulai", value=pd.to_datetime(df[C]).min(), max_value=pd.to_datetime(df[C]).max())
+        tanggal_akhir = st.date_input("Tanggal Akhir", value=pd.to_datetime(df[C]).max(), min_value=pd.to_datetime(df[C]).min())
+        df = df[(df[C] >= pd.to_datetime(tanggal_mulai)) & (df[C] <= pd.to_datetime(tanggal_akhir))]
+    
     
     # Menentukan nilai minimum support
-    minimum_support_percentage = st.number_input("Nilai minimum support: %", min_value=1, max_value=100)
+    minimum_support_percentage = st.number_input("Minimum Support: ( % )", min_value=1, max_value=100)
     minimum_support = minimum_support_percentage / 100
-    minimum_confidence_percentage = st.number_input("Nilai minimum confidence: %", min_value=1, max_value=100)
+    minimum_confidence_percentage = st.number_input("Minimum Confidence: ( % )", min_value=1, max_value=100)
     minimum_confidence = minimum_confidence_percentage / 100
 
    # Menampilkan hasil algoritma apriori
