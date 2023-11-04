65d2c61e82513a3b595ddfaa2221452a876fe3fc
Irfan Novaldo <30534322+aldop07@users.noreply.github.com>
2023-11-04 23:44:17 +0700

    Update apriori.py

--- apriori.py
+++ apriori.py
@@ -62,7 +62,8 @@ if uploaded_file:
 
         # Buat data menjadi binominal
         #tabular_encode = tabular.applymap(hot_encode)
-        tabular_encode = TransactionEncoder(tabular)
+        te = TransactionEncoder()
+        tabular_encode = te.fit(tabular).transform(tabular)
         # Bangun model apriori
         frq_items = apriori(tabular_encode, min_support=minimum_support, use_colnames= True)
 
