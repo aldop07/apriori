afb5190657a8270f64dc7ea64525dd448e0fd934
Irfan Novaldo <30534322+aldop07@users.noreply.github.com>
2024-01-08 06:39:40 +0700

    Update apriori.py

--- apriori.py
+++ apriori.py
@@ -75,7 +75,7 @@ if uploaded_file:
         # Menampilkan hasil algoritma apriori dalam bentuk dataframe
         st.write(f'Ditemukan {len(rules)} Aturan Asosiasi')
         st.dataframe(rules.applymap(lambda x: ', '.join(x) if type(x) == frozenset else x))
-        st.write(f'{transactions}')
+        st.write(f'{dataset}')
     else:
         st.warning("Tidak ada aturan yang diproses")
 else:
