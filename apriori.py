8450598b9cc18a19417cecc597f80cc6e9b03f23
Irfan Novaldo <30534322+aldop07@users.noreply.github.com>
2024-01-11 17:15:19 +0700

    Update apriori.py

--- apriori.py
+++ apriori.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from mlxtend.frequent_patterns import apriori, fpgrowth, association_rules
+from mlxtend.frequent_patterns import apriori, association_rules
 from mlxtend.preprocessing import TransactionEncoder
 
 #IRFAN NOVALDO HUANG
@@ -13,14 +13,22 @@ st.header('Market Basket Analysis')
 # Baca data transaksi dari database
 uploaded_file = st.file_uploader("Pilih file Excel/xlsx yang diupload:")
 if uploaded_file:
-    df_original = pd.read_excel(uploaded_file)
-    index_list = df_original.columns.tolist()
-    A = st.selectbox('Invoice / ID', index_list)
-    B = st.selectbox('Product / Produk', index_list)
-
+    df = pd.read_excel(uploaded_file)
+    index_list = df.columns.tolist()
+    data = st.radio("Pilih Tipe Data", ["Normalisasi","Denormalisasi"])
+    A = st.selectbox('X / Invoice', index_list)
+    B = st.selectbox('Y / Product', index_list)
+    
+    # Mendapatkan daftar unik dari semua kolom
+    data_unik = df.stack().unique()
+    item = st.multiselect('Daftar item unik yang akan di cleaning', data_unik)
+    
+    # Menyaring DataFrame sesuai dengan item yang dipilih
+    df = df[~df.apply(lambda row: any(val in item for val in row), axis=1)]
+    
     # Menentukan nilai minimum support
     minimum_support = st.number_input("Minimum Support: ( % )", 0, max_value=100)
-    minimum_support = minimum_support / 100
+    minimum_support_bagi = minimum_support / 100
     minimum_confidence = st.number_input("Minimum Confidence: ( % )", 0, max_value=100)
     minimum_confidence = minimum_confidence / 100
     
@@ -29,36 +37,39 @@ if uploaded_file:
         st.success('HASIL PERHITUNGAN APRIORI')
 
         # Membuat DataFrame baru hanya dengan kolom A dan B
-        df_original = df_original[[A, B]]
+        df = df[[A, B]]
 
         # Membersihkan nilai yang hilang
-        df_original.dropna(inplace=True)
+        df.dropna(inplace=True)
 
         # Membersihkan nilai yang duplikat
-        df_original.drop_duplicates(inplace=True)
+        df.drop_duplicates(inplace=True)
 
-        if A == B:
-            # Convert the 'Items' column to a list of lists
-            dataset = df_original[f'{A}'].apply(lambda x: [item.strip() for item in x.split(', ')]).tolist()
+        if data == "Denormalisasi":
+            # Konversi kolom 'Items' menjadi list of lists
+            dataset = df[f'{A}'].apply(lambda x: [item.strip() for item in x.split(', ')]).tolist()
         else:
-            # Transform DataFrame to the required format
-            transactions = df_original.groupby(f'{A}')[f'{B}'].apply(list).reset_index(name='Items')
-            # Convert the 'Items' column to a list of lists
+            pass
+        if data == "Normalisasi":
+            # Transformasi DataFrame ke dalam format yang diperlukan
+            transactions = df.groupby(f'{A}')[f'{B}'].apply(list).reset_index(name='Items')
+            # Konversi kolom 'Items' menjadi list of lists
             dataset = transactions['Items'].tolist()
-
+        else:
+            pass
         # Gunakan mlxtend untuk mencari frequent itemsets
         te = TransactionEncoder()
         te_ary = te.fit(dataset).transform(dataset)
         df_transformed = pd.DataFrame(te_ary, columns=te.columns_)
 
         # Bangun model apriori
-        frq_items = apriori(df_transformed, min_support=minimum_support, use_colnames=True)
+        frq_items = apriori(df_transformed, min_support=minimum_support_bagi, use_colnames=True)
 
         # Mengumpulkan aturan dalam dataframe
         rules = association_rules(frq_items, metric="confidence", min_threshold=minimum_confidence)
 
         # Drop lift leverage dan conviction
-        rules = rules.drop(['zhangs_metric','lift', 'leverage', 'conviction'], axis=1)
+        rules = rules.drop(['lift', 'leverage', 'conviction'], axis=1)
 
         # Mengubah nilai support, confidence, dan lift menjadi persentase
         rules[["antecedent support", "consequent support", "support", "confidence"]] = rules[["antecedent support", "consequent support", "support", "confidence"]].applymap(lambda x: "{:.0f}%".format(x * 100))
@@ -67,16 +78,14 @@ if uploaded_file:
         rules = rules.sort_values(['confidence', 'support'], ascending=[False, False])
 
         # Menampilkan frekuensi itemset
-        st.write(f' Terdapat {len(frq_items)} Frekuensi Item')
+        st.write(f'Terdapat {len(frq_items)} Frekuensi Item')
         frq_items = frq_items.sort_values(['support', ], ascending=[False])
         frq_items[["support"]] = frq_items[["support"]].applymap(lambda x: "{:.0f}%".format(x * 100))
         st.dataframe(frq_items.applymap(lambda x: ', '.join(x) if type(x) == frozenset else x))
 
         # Menampilkan hasil algoritma apriori dalam bentuk dataframe
-        st.write(f'Ditemukan {len(rules)} Aturan Asosiasi dari total {len(dataset)} data transaksi')
+        st.write(f'Ditemukan {len(rules)} Aturan Asosiasi dari total {len(dataset)} data  transaksi')
         st.dataframe(rules.applymap(lambda x: ', '.join(x) if type(x) == frozenset else x))
-        #st.write(f'{dataset}')
-        #st.dataframe(df_original)
     else:
         st.warning("Tidak ada aturan yang diproses")
 else:
